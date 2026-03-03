00089de2287bba137e79101bc9c0bf23e04ab156
musicianhub9 <musicianhub9@gmail.com>
2026-03-03 15:41:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1652,5 +1652,4 @@ def find_musicians():
 # ✅ RUN SERVER
 # -------------------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
